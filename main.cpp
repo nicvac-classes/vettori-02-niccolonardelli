2de71f212e7d8a097154825fa381d8a6aa978c92
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2025-01-22 18:57:54 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 using namespace std;
+#include <vector>
+int main()
+{
+    int n, i, voto, nass;
+    string nomi;
+    cout << "quanti alunni ci sono" << endl;
+    cin >> n;
+    vector<int>voti(n);
+    vector<int>assenze(n);
+    vector<string>alunni(n);
+    i=0;
+    while (i<n)
+    {
+        cout << "come si chiama l'alunno?" << endl;
+        cin >> nomi;
+        alunni[i] = nomi;
+        cout << "qual è il voto dell'alunno" << endl;
+        cin >> voto;
+        voti[i] = voto;
+        cout << "quante assenze ha l'alunno?"  << endl;
+        cin >> nass;
+        assenze [i] = nass;
+        i=i+1;
+    }
+    i = 0;
+    while (i< n)
+    {
+        cout << "il voto dello studente di nome " << alunni [i] << " è "  << voti[i] << ". lo studente ha " << assenze[i] << " assenze." << endl; 
+        i=i+1;
+    }
+    i=0;
+    cout << "inserisci l'indice dello studente (primo studente = 0, secondo studente = 1 e cosi via.)" << endl;
+    cin>> i;
+    cout << "il voto dello studente di nome " << alunni [i] << " è "  << voti[i] << ". lo studente ha " << assenze[i] << " assenze." << endl;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
